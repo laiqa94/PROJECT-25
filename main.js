b888f31f82c9cc32f7647534b63357c394a25444
laiqa khan <152205933+laiqa94@users.noreply.github.com>
2024-03-22 02:26:21 +0500

    Add files via upload
    THIS IS MY PROJECT

---
+++ main.js
@@ -0,0 +1,12 @@
+//Create a variable callled alien color
+var alien_color = 'Green';
+// Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+if (alien_color === 'Green') {
+    console.log("player just earned 5 point !");
+}
+// Write one version of this program that passes the if test and another that fails. 
+alien_color = 'yellow';
+// (The version that fails will have no output.)
+if (alien_color === 'yellow') {
+    console.log("Player just earned 5 point !");
+}
---
+++ main.ts
@@ -0,0 +1,17 @@
+
+
+//Create a variable callled alien color
+let alien_color :String ='Green';
+
+// Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+if(alien_color === 'Green'){
+    console.log("player just earned 5 point !");
+}
+
+// Write one version of this program that passes the if test and another that fails. 
+alien_color = 'yellow';
+
+// (The version that fails will have no output.)
+if(alien_color === 'yellow'){
+    console.log("Player just earned 5 point !");
+}
